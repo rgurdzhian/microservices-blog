fad9f6f8e111c183622ff63422af554add8356e9
Ruben Gurdzhian <ruben.gurdzhian@gmail.com>
2021-12-24 23:36:21 +0200

    add posts and comments services

---
+++ posts/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    }
+
+    console.log(`Post Created - ${title}`);
+    res.status(201).send(posts[id]);
+})
+
+app.listen(4000, () => console.log("Posts service listening on port 4000"));
