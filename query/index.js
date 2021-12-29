ba7ab7aad3f0f79af004da30493f64c6ead06964
Ruben Gurdzhian <ruben.gurdzhian@gmail.com>
2021-12-29 11:20:39 +0200

    implement query service and post data strcuture

--- query/index.js
+++ query/index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const postsWithComments = [];
+const postsWithComments = {};
 
 app.get('/posts', (req, res) => {
     res.send(postsWithComments);
 })
 
 app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        postsWithComments[id] = {
+            id,
+            title,
+            comments: []
+        }
+    }
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+        const post = postsWithComments[postId];
 
+        post.comments.push({ id, content });
+    }
+
+    res.send({});
+})
+
+app.listen(4002, () => {
+    console.log('Query service is listening on port 4002');
 })
